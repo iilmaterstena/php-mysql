a9a1dcca651fd78a10a17480908c20a8a1638f52
iilmaterstena <ilmaterstena860@gmail.com>
2026-01-17 14:46:49 +0100

    day 2

---
+++ moduli2/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$school_Name = "Digital School";
+
+echo("i go to $school_Name")."<hr>";
+
+$numri1 = 2;
+
+echo("i am in level number $numri1")."<br>";
+
+$the_String = "ju jeni klasa ma e mira";
+
+echo strlen($the_String)."<hr>";
+
+echo str_word_count($the_String)."<hr>";
+
+echo str_replace("mira", "zhurmshmja", $the_String)."<hr>";
+
+echo strrev($the_String)."<hr>";
+
+?>
+
